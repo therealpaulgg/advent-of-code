a63518a96d71fee74af36c81883b7dda6c4a67b8
Paul Gellai <paul.gellai@carvana.com>
2022-12-01 15:04:10 -0700

    more efficient memory solution

--- day1/sol.py
+++ day1/sol.py
@@ -1,16 +1,19 @@
 import re
 
 inp = open("input.txt", "r")
-text = inp.readlines()
-res = []
-startIndex = 0
+currentMax = None
 endIndex = 0
+currentLines = []
 
-for line in text:
+for line in inp:
+    currentLines.append(line)
     if re.match(r"^\n", line):
-        caloriesList = text[startIndex:endIndex]
-        res.append(sum(map(lambda x: int(x.strip()), caloriesList)))
-        startIndex = endIndex + 1
+        caloriesList = currentLines[0:endIndex - 1]
+        summation = sum(map(lambda x: int(x.strip()), caloriesList))
+        if currentMax is None or currentMax < summation:
+            currentMax = summation
+        endIndex = 0
+        currentLines = []
     endIndex += 1
-    
-print(max(res))
+
+print(currentMax)
